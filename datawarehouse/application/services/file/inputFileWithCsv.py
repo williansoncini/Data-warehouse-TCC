cd47e9b894fdb1705cfa304fad53056c3270a6e0
Willian <willian_soncini@hotmail.com>
2021-08-10 00:04:16 -0300

    Importação de arquivos CSV sendo feitos

--- datawarehouse/application/services/file/inputFileWithCsv.py
+++ datawarehouse/application/services/file/inputFileWithCsv.py
@@ -8,15 +8,18 @@ module_dir = os.path.dirname(__file__)  # get current directory
 def importCSVfile(nameFile):
     conn = connect()
 
-    file_path = os.path.join(module_dir, '..TESTE.CSV')
-    print('CAMINHO DO ARQUIVO:', file_path)
+    # file_path = os.path.join(module_dir, '..TESTE.CSV')
+    # open(str(nameFile),'rb')
+    # print('nameFile:',nameFile)
+    # print('CAMINHO DO ARQUIVO:', file_path)
     
     # fs = FileSystemStorage()
     # file = fs.open(nameFile,'rb')
-    # cur = conn.cursor()
-    # file = open(nameFile, 'r')
-    # cur.copy_from(file,'teste_csv',sep=';')
+    cur = conn.cursor()
+    file = open(nameFile, 'r')
+    cur.copy_from(file,'teste_csv',sep=';')
+    cur.close()
 
-
+    conn.commit()
     print('conexão sendo fechada!')
     conn.close()
--- datawarehouse/application/views.py
+++ datawarehouse/application/views.py
@@ -1,9 +1,9 @@
-from django.http.response import HttpResponse, HttpResponseRedirect
+# from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render,get_list_or_404
-from .models import Datamart
-from .forms import fileForm
-from .services.file.fileService import handle_uploaded_file
-from django.contrib import messages
+# from .models import Datamart
+# from .forms import fileForm
+# from .services.file.fileService import handle_uploaded_file
+# from django.contrib import messages
 from django.core.files.storage import FileSystemStorage
 from .services.file.inputFileWithCsv import importCSVfile
 
@@ -19,15 +19,16 @@ def fileinput(request):
         # file = fileForm(request.POST, request.FILES)
 
         file = request.FILES['document']
-        print(file.name)
-        print(file.size)
-        print(file.content_type)
+        # print(file.name)
+        # print(file.size)
+        # print(file.content_type)
 
         fs = FileSystemStorage()
         name = fs.save(file.name, file)
+        # print('nome:',name)
         url = fs.url(name)
 
-        importCSVfile(url)
+        importCSVfile(str(url[1:]))
         # if file.is_valid():
         #     handle_uploaded_file(request.FILES['document']) 
         #     messages.success(request,'Arquivo carregado com sucesso')
