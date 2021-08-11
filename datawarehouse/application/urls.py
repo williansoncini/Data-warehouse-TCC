3f2122179993b5f5630fdf0612491a91edcf80fa
Willian <willian_soncini@hotmail.com>
2021-08-10 21:30:31 -0300

    Importando arquivo CSV a partir de query

--- datawarehouse/application/urls.py
+++ datawarehouse/application/urls.py
@@ -5,6 +5,7 @@ app_name = 'application'
 
 urlpatterns = [
     path('',views.datamart_list, name='datamart_list'),
-    path('/file', views.queryInput, name='file_input'),
+    path('/teste/file', views.fileinput, name='file_input'),
+    path('/teste/query', views.queryInput, name='query_input'),
     # path('/file', views.fileinput, name='file_input'),
 ]
